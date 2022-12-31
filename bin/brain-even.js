00c5f8483c781798480bbd5d424747e838832e82
Ramazanov Vitalii <zapupenec@gmail.com>
2022-12-31 18:27:18 +0300

    refactor bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -15,15 +15,10 @@ for (let i = 1; i <= countQuestion; i += 1) {
   const randomNumber = Math.floor(Math.random() * endRange);
   console.log(`Qustion: ${randomNumber}`);
   const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
 
-  if (randomNumber % 2 === 0 && answer !== 'yes') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
-  }
-
-  if (randomNumber % 2 !== 0 && answer !== 'no') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+  if ((randomNumber % 2 === 0 && answer !== 'yes') || (randomNumber % 2 !== 0 && answer !== 'no')) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
     console.log(`Let's try again, ${userName}!`);
     break;
   }
